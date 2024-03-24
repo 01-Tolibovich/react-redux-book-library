23764d80a5e8978e0182f40349f54ae1bdb82200
Daler Abdurahimov <01.tolibovich@gmail.com>
2024-03-24 10:40:49 +0500

    highlight filter text matches

--- frontend/src/components/bookList/BookList.jsx
+++ frontend/src/components/bookList/BookList.jsx
@@ -1,16 +1,20 @@
 import { useSelector, useDispatch } from "react-redux";
 import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
-import { selectAuthorFilter, selectTitleFilter, selectOnlyFavorite } from "../../redux/slices/filterSlice";
+import {
+  selectAuthorFilter,
+  selectTitleFilter,
+  selectOnlyFavorite,
+} from "../../redux/slices/filterSlice";
 
-import { BsBookmarkStar, BsBookmarkStarFill  } from "react-icons/bs"
+import { BsBookmarkStar, BsBookmarkStarFill } from "react-icons/bs";
 
 import "./styles.css";
 
 const BookList = () => {
-  const books = useSelector(state => state.books)
-  const filterByTitile = useSelector(selectTitleFilter)
-  const filterByAuthor = useSelector(selectAuthorFilter)
-  const filterOnlyFavorite = useSelector(selectOnlyFavorite)
+  const books = useSelector((state) => state.books);
+  const filterByTitile = useSelector(selectTitleFilter);
+  const filterByAuthor = useSelector(selectAuthorFilter);
+  const filterOnlyFavorite = useSelector(selectOnlyFavorite);
   const dispatch = useDispatch();
 
   const deleteHandle = (id) => {
@@ -18,8 +22,8 @@ const BookList = () => {
   };
 
   const toggleFavoriteHandle = (id) => {
-    dispatch(toggleFavorite(id))
-  }
+    dispatch(toggleFavorite(id));
+  };
 
   const filteredBooks = books.filter((book) => {
     const matchesTitle = book.title
@@ -33,7 +37,24 @@ const BookList = () => {
     const matchesFavorite = filterOnlyFavorite ? book.isFavorite : true;
 
     return matchesTitle && matchesAuthor && matchesFavorite;
-  })
+  });
+
+  const highlightMatch = (text, filter) => {
+    if (!filter) return text;
+
+    const regex = new RegExp(`(${filter})`, "gi");
+
+    return text.split(regex).map((substring, index) => {
+      if (substring.toLowerCase() === filter.toLowerCase()) {
+        return (
+          <span key={index} className="highlight">
+            {substring}
+          </span>
+        );
+      }
+      return substring;
+    });
+  };
 
   return (
     <div className="app-block book-list">
@@ -45,17 +66,26 @@ const BookList = () => {
           {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
-                {++i}) {book.title} by <strong>{book.author}</strong>
+                {++i}) {highlightMatch(book.title, filterByTitile)} by{" "}
+                <strong>{highlightMatch(book.author, filterByAuthor)}</strong>
               </div>
               <div className="book-actions">
-                <span role="button" onClick={() => toggleFavoriteHandle(book.id)}>
-                {book.isFavorite ? (
-                  <BsBookmarkStarFill className="star-icon" />
-                ) : (
-                  <BsBookmarkStar className="star-icon" />
-                )}
+                <span
+                  role="button"
+                  onClick={() => toggleFavoriteHandle(book.id)}
+                >
+                  {book.isFavorite ? (
+                    <BsBookmarkStarFill className="star-icon" />
+                  ) : (
+                    <BsBookmarkStar className="star-icon" />
+                  )}
                 </span>
-                <button onClick={() => deleteHandle(book.id)} className="book-actions">Delete</button>
+                <button
+                  onClick={() => deleteHandle(book.id)}
+                  className="book-actions"
+                >
+                  Delete
+                </button>
               </div>
             </li>
           ))}
